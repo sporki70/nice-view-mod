6f6e0ba9abe46d83a2b8d91a4b2e7d4c2a4db269
sporki70 <56455561+sporki70@users.noreply.github.com>
2026-08-16 16:25:33 +0200

    Modify peripheral status widget drawing logic
    Updated the peripheral status widget to include a test label and modified the drawing logic.

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -52,7 +52,8 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
                         state->connected ? LV_SYMBOL_WIFI : LV_SYMBOL_CLOSE);
 
     // Rotate canvas
-    rotate_canvas(canvas, cbuf);
+    //rotate_canvas(canvas, cbuf);
+    lv_canvas_draw_text(canvas, 0, 25, CANVAS_SIZE, &label_dsc, "TEST");
 }
 
 static void set_battery_status(struct zmk_widget_status *widget,
