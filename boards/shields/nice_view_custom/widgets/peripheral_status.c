2d0d85bd3d2b4c3a760fd678d36a097db99b6cbf
sporki70 <56455561+sporki70@users.noreply.github.com>
2026-08-16 21:04:48 +0200

    Refactor peripheral_status.c to improve clarity

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -26,7 +26,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(jelllyfish);
 LV_IMG_DECLARE(seahorse);
-LV_IMG_DECLARE(CHAR_PIKACHU);
+LV_IMG_DECLARE(char_pikachu);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -126,10 +126,10 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
         &balloon,
         &jelllyfish,
         &seahorse,
-        &CHAR_PIKACHU
+        &char_pikachu
     };
 
-    lv_img_set_src(art, &CHAR_PIKACHU);
+    lv_img_set_src(art, &char_pikachu);
     
     /*
      * 68px status + 92px artwork = 160px total
