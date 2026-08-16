5830aef14ead31fe9b3a2d9dadfa8e03082a62cb
sporki70 <56455561+sporki70@users.noreply.github.com>
2026-08-16 20:07:46 +0200

    Refactor peripheral status widget and add artwork

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -26,6 +26,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(jelllyfish);
 LV_IMG_DECLARE(seahorse);
+LV_IMG_DECLARE(char_pikachu);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
